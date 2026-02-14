bbf2fb6bc535f449fd0a52f17a4c4475bb566cd6
Agwu godswill uguru <godswilluguru334@gmail.com>
2026-02-14 21:23:38 +0100

    first init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("RankUp CBT API Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
