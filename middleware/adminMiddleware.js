ae19cfc23bb8587431b6436786a06288aea7a8f7
Agwu godswill uguru <godswilluguru334@gmail.com>
2026-02-28 21:46:08 +0100

    et permission to acces admin in adminmidllesware and also in mongodb compass

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,5 +1,5 @@
-module.exports = function (req, res, next) {
-  if (req.user && req.user.role === "admin") {
+exports.adminOnly = (req, res, next) => {
+  if (req.user && req.user.isAdmin === true) {
     next();
   } else {
     return res.status(403).json({ message: "Admin only route" });
