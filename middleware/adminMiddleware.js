b5fd172b960255753de6767a5a7743480b50ff69
Agwu godswill uguru <godswilluguru334@gmail.com>
2026-03-01 21:39:33 +0100

    duplicated admin

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -5,3 +5,11 @@ exports.adminOnly = (req, res, next) => {
     return res.status(403).json({ message: "Admin only route" });
   }
 };
+
+exports.adminOnly = (req, res, next) => {
+  if (req.user && req.user.isAdmin === true) {
+    next();
+  } else {
+    return res.status(403).json({ message: "Admin only route" });
+  }
+};
